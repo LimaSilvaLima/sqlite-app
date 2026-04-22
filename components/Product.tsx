2257dbede0f55508652086cd49cbd5ac49e1bcb5
Jose Silva Lima <limasilvalima@hotmail.com>
2026-04-22 19:35:11 -0300

    estilizada a lista de proutos

--- components/Product.tsx
+++ components/Product.tsx
@@ -8,8 +8,16 @@ type Props = PressableProps & {
     
 }
 
-export function Product({ data, ...rest }: Props){
-    return<Pressable {...rest}>
-        <Text>{data.name} - {data.quantity}</Text>
-    </Pressable>
+export function Product({ data, style, ...rest }: Props) {
+    return (
+        <Pressable
+            style={[
+                { backgroundColor: "#CECECE", padding: 24, gap: 12, borderRadius: 5, flexDirection: 'row' },
+                
+            ]}
+            {...rest}
+        >
+            <Text>{data.name} - {data.quantity}</Text>
+        </Pressable>
+    );
 }
